8b5cb27a4008836594fb9441da611bf0b4af91ce
Jean <nino.jeanrobert@gmail.com>
2016-06-20 20:44:10 +0000

    rev

--- server.js
+++ server.js
@@ -8,9 +8,19 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-app.use(express.static(path.resolve(__dirname, 'client')));
 
-app.get('')
+app.route('/').get(function(req, res) {
+      var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+     var info = {
+         'ip-address': ip,
+         'language': req.headers["accept-language"].split(',')[0],
+         'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
+     };
+     res.send(info);
+    });
 
 
 app.listen(port, function(){
